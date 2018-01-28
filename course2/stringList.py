dfd72106bf273d9b0cb7130f93f7b5a2725e9013
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-01-28 11:41:49 +0200

    stringList.py

--- course2/stringList.py
+++ course2/stringList.py
@@ -1,5 +1,9 @@
 names = "Do you know what you want?"
-namesList = names.split();
+namesList = names.split()
 for name in namesList:
 	print(name)
 
+words = "Mohamed;Ahmed;Sami;Ronald;Regan"
+wordsList = words.split(';')
+for word in wordList:
+	print(word)
